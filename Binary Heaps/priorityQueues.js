7bbeee9212173ab0307c374c4937c2b35d558754
hadis ghafouri <hadisghafouri98@gmail.com>
2021-05-21 20:29:18 +0430

    add binary heap folder and add priority queue class

---
+++ Binary Heaps/priorityQueues.js
@@ -0,0 +1,27 @@
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+    enqueue(val, priority) {
+        this.values.push({ val, priority });
+        this.sort();
+    }
+    dequeue() {
+        return this.values.shift();
+    }
+    sort() {
+        // this.values.sort((a, b) => a.priority - b.priority);
+        this.values.sort((a, b) => {
+            return (a.priority - b.priority)
+        });
+    }
+}
+
+const priorityQueue = new PriorityQueue();
+priorityQueue.enqueue('A', 0);
+priorityQueue.enqueue('B', 5);
+priorityQueue.enqueue('U', 3);
+priorityQueue.enqueue('W', 9);
+priorityQueue.enqueue('P', 8);
+priorityQueue.enqueue('Z', 2);
+console.log(priorityQueue.values);
