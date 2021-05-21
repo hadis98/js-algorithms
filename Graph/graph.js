787beb5003df990d2a4dd094534093c0bb70fcfb
hadis ghafouri <hadisghafouri98@gmail.com>
2021-05-21 13:30:04 +0430

    add 2 removeEdge methods to graph class

--- Graph/graph.js
+++ Graph/graph.js
@@ -12,6 +12,20 @@ class graph {
         this.adjacencyList[vertex1].push(vertex2);
         this.adjacencyList[vertex2].push(vertex1);
     }
+    removeEdge(vertex1, vertex2) {
+        const index = this.adjacencyList[vertex1].indexOf(vertex2);
+        this.adjacencyList[vertex1].splice(index, 1);
+        const index2 = this.adjacencyList[vertex2].indexOf(vertex1);
+        this.adjacencyList[vertex2].splice(index2, 1);
+    }
+    removeEdge2(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+            v => v !== v2
+        );
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+            v => v !== v1
+        );
+    }
 }
 
 let g = new graph();
@@ -22,4 +36,6 @@ g.addVertex('Italy');
 console.log(g);
 g.addEdge('America', 'Germany');
 g.addEdge('America', 'Italy');
+console.log(g);
+g.removeEdge2('America', 'Germany');
 console.log(g);
