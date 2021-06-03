8abc8e663330c5065d0925b2e58640e11a27e0b9
hadis ghafouri <hadisghafouri98@gmail.com>
2021-06-03 15:23:01 +0430

    add dequeue test in priority queue

--- Binary Heaps/priorityQueues.js
+++ Binary Heaps/priorityQueues.js
@@ -25,3 +25,5 @@ priorityQueue.enqueue('W', 9);
 priorityQueue.enqueue('P', 8);
 priorityQueue.enqueue('Z', 2);
 console.log(priorityQueue.values);
+priorityQueue.dequeue();
+console.log(priorityQueue.values); //delete the element with smallest priority
