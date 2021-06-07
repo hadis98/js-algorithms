0a82d13fc8187a8b37daa395c224ededcedc0d88
hadis ghafouri <hadisghafouri98@gmail.com>
2021-06-07 23:42:14 +0430

    add extractMax method to max_binary_heap class

--- Binary Heaps/max_binary_heap.js
+++ Binary Heaps/max_binary_heap.js
@@ -5,12 +5,51 @@ class MaxBinaryHeap {
     insert(value) {
         this.values.push(value);
         let curIdx = this.values.length - 1;
-        let parentIdx = Math.floor(((curIdx) - 1) / 2);
+        let parentIdx = Math.floor((curIdx - 1) / 2);
         while (this.values[curIdx] > this.values[parentIdx]) {
-            [this.values[curIdx], this.values[parentIdx]] = [this.values[parentIdx], this.values[curIdx]];
+            [this.values[curIdx], this.values[parentIdx]] = [
+                this.values[parentIdx],
+                this.values[curIdx],
+            ];
             curIdx = parentIdx;
-            parentIdx = Math.floor(((curIdx) - 1) / 2);
+            parentIdx = Math.floor((curIdx - 1) / 2);
         }
+    }
+
+    extractMax() {
+        //remove root
+        [this.values[0], this.values[this.values.length - 1]] = [
+            this.values[this.values.length - 1],
+            this.values[0],
+        ];
+        let oldRoot = this.values.pop();
+        let curIdx = 0;
+        let child_leftIdx = 2 * curIdx + 1;
+        let child_rightIdx = 2 * curIdx + 2;
+        while (
+            (this.values[curIdx] < this.values[child_leftIdx] ||
+                this.values[curIdx] < this.values[child_rightIdx]) &&
+            child_rightIdx <= this.values.length - 1 &&
+            child_leftIdx <= this.values.length - 1
+        ) {
+            if (this.values[child_leftIdx] > this.values[child_rightIdx]) {
+                [this.values[curIdx], this.values[child_leftIdx]] = [
+                    this.values[child_leftIdx],
+                    this.values[curIdx],
+                ];
+                curIdx = child_leftIdx;
+                child_leftIdx = 2 * curIdx + 1;
+            } else {
+                [this.values[curIdx], this.values[child_rightIdx]] = [
+                    this.values[child_rightIdx],
+                    this.values[curIdx],
+                ];
+                curIdx = child_rightIdx;
+                child_rightIdx = 2 * curIdx + 2;
+            }
+        }
+
+        return oldRoot;
     }
 }
 
@@ -22,4 +61,14 @@ heap.insert(38);
 heap.insert(45);
 console.log(heap);
 heap.insert(200);
+console.log(heap);
+console.log(heap.extractMax());
+console.log(heap);
+console.log(heap.extractMax());
+console.log(heap);
+console.log(heap.extractMax());
+console.log(heap);
+console.log(heap.extractMax());
+console.log(heap);
+console.log(heap.extractMax());
 console.log(heap);
