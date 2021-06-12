6dc1788552cc0159d0015838fd3bad92b3025b18
hadis ghafouri <hadisghafouri98@gmail.com>
2021-06-12 15:19:11 +0430

    create Node and linkedList class

---
+++ singly_linkedList/singly_linked_list.js
@@ -0,0 +1,21 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class singlyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
+
+// const first = new Node("hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node(" you?");
+// console.log(first);
